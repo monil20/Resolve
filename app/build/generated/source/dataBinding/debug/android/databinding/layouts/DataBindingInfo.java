2d04e0bb5ecd3659556066bb76d462d018166655
monil20 <monilcoolguy@gmail.com>
2018-05-03 19:49:56 +0530

    Splash, Intro, Login, Signup, Home, Submit Issue, My Cases, Browse Issues, Single Issue

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="14d1b254-48bb-4fa2-908c-26a06bf00a4a")
+public class DataBindingInfo {}
